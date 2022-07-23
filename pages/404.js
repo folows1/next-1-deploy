9bf7f2a1a94acb94134001165a39b1de04ef874f
folows1 <michelribeirocorrea@gmail.com>
2022-07-23 00:32:40 -0300

    c

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function PageNotFound() {
+    return (
+        <div>
+            <h1>404</h1>
+            <Link href="/">
+                Home
+            </Link>
+        </div>
+    )
+}
